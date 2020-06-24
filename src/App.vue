b9bbaa800ab665afa8ec3f2f79210ba8051cca42
솔 <sol@sol-ui-iMac.local>
2020-06-24 15:30:44 +0900

    todolist 추가하기 및 생성

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,35 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <TodoInput v-on:addlist="addList"></TodoInput>
+    <TodoList :todoList="items"></TodoList>
+  </div>
 </template>
 
 <script>
-export default {};
+import TodoInput from "@/components/TodoInput";
+import TodoList from "@/components/TodoList";
+
+export default {
+  components: { TodoInput, TodoList },
+  data() {
+    return {
+      items: []
+    };
+  },
+  methods: {
+    addList(data) {
+      this.items.push(data);
+      console.log(this.items);
+    }
+  }
+};
 </script>
 
 <style>
+#app {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
 </style>
