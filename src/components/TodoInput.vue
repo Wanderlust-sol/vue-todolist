5805234bc5b484795f351b9c5cf4cd72fb0b36eb
솔 <sol@sol-ui-iMac.local>
2020-06-24 18:12:09 +0900

    수정기능 추가중

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -1,15 +1,19 @@
 <template>
   <div>
     <input v-model="todo" type="text" placeholder="할일을 적어주세요." v-on:keydown.enter="addButton" />
-    <button @click="addButton">추가</button>
+    <button v-if="!toggle" @click="addButton">추가</button>
+    <button v-else @click="applyBtn">적용</button>
   </div>
 </template>
 
 <script>
+import { EventBus } from "@/event-bus";
+
 export default {
   data() {
     return {
-      todo: ""
+      todo: "",
+      toggle: false
     };
   },
   methods: {
@@ -18,7 +22,22 @@ export default {
         this.$emit("addlist", this.todo);
         this.todo = "";
       } else return alert("내용을 입력해주세요");
+    },
+    applyBtn() {
+      //   EventBus.$on("edititem", function(item) {
+      //     console.log(item);
+      //   });
+      console.log("hi?");
     }
+  },
+  created() {
+    EventBus.$on("edititem", item => {
+      this.todo = item;
+      this.toggle = true;
+    });
+  },
+  beforeDestroy() {
+    EventBus.$off("edititem");
   }
 };
 </script>
